286bd634bad52b4a1d7a7341fbea5c1bebf0749d
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-10-23 10:20:35 +0700

    Add files via upload

---
+++ pages/identitas/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2 class="text-start my-4">{{ siswa.nama }}</h2>
+      <div class="col-md-12">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">id : {{  siswa.id }}</li>
+          <li class="list-group-item">nis : {{  siswa.nis }}</li>
+          <li class="list-group-item">nama : {{ siswa.nama }}</li>
+          <li class=" list-group-item">jenis_kelamin : {{ siswa.jenis_kelamin }}</li>
+          <li class=" list-group-item">alamat : {{ siswa.alamat }}</li>
+        </ul>
+      </div>
+  </div>
+</template>
+
+<script setup>
+
+const supabase = useSupabaseClient()
+const siswa = ref([])
+
+const getSiswaById = async () => {
+    const { data, error } = await supabase
+    .from('siswa')
+    .select(`*`)
+    if(data) siswa.value = data
+}
+
+onMounted(() => {
+    getSiswaByID()
+})
+</script>

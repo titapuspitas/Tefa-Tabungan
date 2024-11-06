fb8211a14014d6c1418e0119a9499fbc24adf825
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-11-06 10:06:48 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar fixed">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Selamat Datang Di Tabungan {{ judul }}</span>
+      </div>
+    </nav>
+</template>
+
+<script setup>
+defineProps({
+  judul: String
+})
+</script>
+
+<style scoped>
+.navbar{
+  background: #1A9EA7;
+  height: 15vh;
+}
+.navbar-brand{
+  color: white;
+  font-size: 40px;
+}
+</style>
+

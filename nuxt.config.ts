b495c966e75f5c011fa1027700faf7116e1daf1b
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-11-06 10:08:29 +0700

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  css: [
+      "bootstrap/dist/css/bootstrap.min.css",
+      "bootstrap-icons/font/bootstrap-icons.min.css"
+  ],
+
   modules: ["@nuxtjs/supabase"],
   supabase: {
-    redirect: true
+    redirect:true
   }
 })

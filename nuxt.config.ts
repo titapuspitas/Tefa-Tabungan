79f1c61ceab60b8f36fcb2b717d307cc5f89061a
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-11-18 15:10:20 +0700

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   css: [
       "bootstrap/dist/css/bootstrap.min.css",
       "bootstrap-icons/font/bootstrap-icons.min.css"
